f5ba4087813860c7170b716fce230e785e686de9
Darren Labey <dlabey@fastmail.com>
2019-07-17 23:36:05 -0700

    More stuff

--- handler/auditor.go
+++ handler/auditor.go
@@ -3,7 +3,13 @@ package main
 import (
 	"context"
 	"github.com/aws/aws-lambda-go/lambda"
+	"log"
+	"sync"
 )
+
+type LambdaEvent struct {
+	Records []CloudTrailEvent `json:"Records"`
+}
 
 type CloudTrailEvent struct {
 	ErrorCode         string            `json:"errorCode"`
@@ -16,8 +22,36 @@ type CloudTrailEvent struct {
 	UserIdentity      UserIdentity      `json:"userIdentity"`
 }
 
-func HandleRequest(ctx context.Context) (string, error) {
-	// todo: add logic to switch between EventType
+func HandleRequest(event LambdaEvent, ctx context.Context) (string, error) {
+	recordsLength := len(event.Records)
+	var waitGroup sync.WaitGroup
+	waitGroup.Add(recordsLength)
+
+	for i := 0; i < recordsLength; i++ {
+		go func(i int) {
+			defer waitGroup.Done()
+
+			cloudWatchEvent := event.Records[i]
+			switch eventType := cloudWatchEvent.EventType; eventType {
+			case "AttachRolePolicy":
+			case "CreatePolicy":
+			case "CreatePolicyVersion":
+			case "CreateRole":
+			case "DeletePolicy":
+			case "DeletePolicyVersion":
+			case "DeleteRole":
+			case "DeleteRolePolicy":
+			case "DetachRolePolicy":
+			case "PutRolePolicy":
+			case "SetDefaultPolicyVersion":
+			case "UpdateRole":
+			default:
+				log.Print("EventType not supported:")
+			}
+		}(i)
+	}
+
+	waitGroup.Wait()
 }
 
 func main() {
