f6321e6f0ff3b628852538f438595ce5aa3bb6b0
Darren Labey <dlabey@fastmail.com>
2019-07-22 21:59:04 -0700

    Adding some more stuff and changing some stuff

--- handler/auditor.go
+++ handler/auditor.go
@@ -27,31 +27,49 @@ func HandleRequest(event LambdaEvent, ctx context.Context) (string, error) {
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(recordsLength)
 
+	// if recordLength > 1 checkout project and continue
+
 	for i := 0; i < recordsLength; i++ {
 		go func(i int) {
 			defer waitGroup.Done()
 
 			cloudWatchEvent := event.Records[i]
-			switch eventType := cloudWatchEvent.EventType; eventType {
-			case "AttachRolePolicy":
-			case "CreatePolicy":
-			case "CreatePolicyVersion":
-			case "CreateRole":
-			case "DeletePolicy":
-			case "DeletePolicyVersion":
-			case "DeleteRole":
-			case "DeleteRolePolicy":
-			case "DetachRolePolicy":
-			case "PutRolePolicy":
-			case "SetDefaultPolicyVersion":
-			case "UpdateRole":
-			default:
+			supportedEventTypes := []string{
+				"attachRolePolicy",
+				"CreatePolicy",
+				"CreatePolicyVersion",
+				"CreateRole",
+				"DeletePolicy",
+				"DeletePolicyVersion",
+				"DeleteRole",
+				"DeleteRolePolicy",
+				"DetachRolePolicy",
+				"PutRolePolicy",
+				"SetDefaultPolicyVersion",
+				"UpdateRole",
+			}
+
+			if contains(supportedEventTypes, cloudWatchEvent.EventType) {
+				// queue up a commit for the change for a Git commit
+			} else {
 				log.Print("EventType not supported:")
 			}
 		}(i)
 	}
-
 	waitGroup.Wait()
+
+	// commit all the changes to git
+
+	return "end", nil
+}
+
+func contains(strs []string, str string) bool {
+	for _, contains := range strs {
+		if contains == str {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
