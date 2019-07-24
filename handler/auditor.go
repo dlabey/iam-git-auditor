71d7fa5d5ceb5df8d44a97718c32cb852639be42
Darren Labey <dlabey@fastmail.com>
2019-07-23 22:46:17 -0700

    More progress

--- handler/auditor.go
+++ handler/auditor.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/joho/godotenv"
+	"gopkg.in/src-d/go-git.v4"
+	"gopkg.in/src-d/go-git.v4/storage/memory"
 	"log"
+	"os"
+	"strings"
 	"sync"
 )
 
@@ -22,8 +28,22 @@ type CloudTrailEvent struct {
 	UserIdentity      UserIdentity      `json:"userIdentity"`
 }
 
-func HandleRequest(event LambdaEvent, ctx context.Context) (string, error) {
-	recordsLength := len(event.Records)
+func HandleRequest(evt LambdaEvent, ctx context.Context) (*string, error) {
+	// Initialize dotenv
+	err := godotenv.Load()
+	checkError(err, fmt.Sprintf("Error loading .env file: %s", err))
+
+	// Checkout the audit repo
+	gitRepoUrl := os.Getenv("GIT_REPO")
+	gitRepo, err := git.PlainClone(os.TempDir(), false, &git.CloneOptions{
+		URL:      gitRepoUrl,
+		Progress: os.Stdout,
+	})
+	checkError(err, fmt.Sprintf("Error cloning git repo: %s", err))
+	workTree, err := gitRepo.Worktree()
+	checkError(err, fmt.Sprintf("Error cloning git repo: %s", err))
+
+	recordsLength := len(evt.Records)
 	var waitGroup sync.WaitGroup
 	waitGroup.Add(recordsLength)
 
@@ -33,26 +53,39 @@ func HandleRequest(event LambdaEvent, ctx context.Context) (string, error) {
 		go func(i int) {
 			defer waitGroup.Done()
 
-			cloudWatchEvent := event.Records[i]
-			supportedEventTypes := []string{
-				"attachRolePolicy",
-				"CreatePolicy",
-				"CreatePolicyVersion",
-				"CreateRole",
-				"DeletePolicy",
-				"DeletePolicyVersion",
-				"DeleteRole",
-				"DeleteRolePolicy",
-				"DetachRolePolicy",
-				"PutRolePolicy",
-				"SetDefaultPolicyVersion",
-				"UpdateRole",
-			}
-
-			if contains(supportedEventTypes, cloudWatchEvent.EventType) {
-				// queue up a commit for the change for a Git commit
-			} else {
-				log.Print("EventType not supported:")
+			cloudWatchEvt := evt.Records[i]
+			switch eventName := cloudWatchEvt.EventName; eventName {
+			case "AttachRolePolicy":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+			case "CreatePolicy":
+			case "CreatePolicyVersion":
+			case "CreateRole":
+				errMsg := "Error auditing CreateRole evt: %s"
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+				err = os.Mkdir(roleDir, os.ModeDir)
+				checkError(err, fmt.Sprintf(errMsg, err))
+				inlinePolicyFile := roleDir + "/" + cloudWatchEvt.RequestParameters.RoleName
+				_, err = os.Create(inlinePolicyFile)
+				checkError(err, fmt.Sprintf(errMsg, err))
+				_, err = workTree.Add(inlinePolicyFile)
+				checkError(err, fmt.Sprintf(errMsg, err))
+			case "DeletePolicy":
+			case "DeletePolicyVersion":
+			case "DeleteRole":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+				err := os.RemoveAll(roleDir)
+				checkError(err, fmt.Sprintf("Error auditing DeleteRole evt: %s", err))
+			case "DeleteRolePolicy":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+			case "DetachRolePolicy":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+			case "PutRolePolicy":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+			case "SetDefaultPolicyVersion":
+			case "UpdateRole":
+				roleDir := os.TempDir() + "/" + cloudWatchEvt.RequestParameters.RoleName
+			default:
+				log.Print("EventName not supported:")
 			}
 		}(i)
 	}
@@ -63,13 +96,10 @@ func HandleRequest(event LambdaEvent, ctx context.Context) (string, error) {
 	return "end", nil
 }
 
-func contains(strs []string, str string) bool {
-	for _, contains := range strs {
-		if contains == str {
-			return true
-		}
+func checkError(err error, msg string) {
+	if err != nil {
+		log.Panic(msg)
 	}
-	return false
 }
 
 func main() {
